e32e8aea4da3df89da6a18d9fb6fc815c5b3398a
adam <adam@adam.pl>
2022-10-04 22:19:01 +0200

    ostatni commit

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -38,3 +38,40 @@ def new_topic(request):
     #wyświetlanie pustego formularza
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Dodanie nowego wpisu dla określonego tematu"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #nie przekazano żadnych danych żeby utworzyć formularz
+        form = EntryForm()
+    else:
+        #przekazano dane za pomocą żądania POST, należy je przetworzyć
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    #wyświetlanie pustego formularza
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+def edit_entry(request, entry_id):
+    """"edycja istniejącego już wpisu"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #żądanie początkowe, wypełnienie formularza aktualną treścią wpisu
+        form = EntryForm(instance=entry)
+    else:
+        #przekazano dane za pomocą żądania POST, należy je przetworzyć
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic':topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html', context)
